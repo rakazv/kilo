e2591635e051245abe89b8b313d263811b67e861
rakazv <rakazv@outlook.com>
2024-10-11 05:06:28 +0900

    quit confirmation

--- kilo.c
+++ kilo.c
@@ -20,6 +20,7 @@
 /*** defines ***/
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -559,6 +560,8 @@ void editorMoveCursor(int key) {
 }
 
 void editorProcessKeypress() {
+  static int quit_times = KILO_QUIT_TIMES;
+
   int c = editorReadKey();
 
   switch (c) {
@@ -566,6 +569,13 @@ void editorProcessKeypress() {
     /* TODO */
     break;
   case CTRL_KEY('q'):
+    if (E.dirty && quit_times > 0) {
+      editorSetStatusMessage("WARNING!!! File has unsaved changes. Press "
+                             "Ctrl-Q %d more times to quit.",
+                             quit_times);
+      quit_times--;
+      return;
+    }
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
@@ -623,6 +633,8 @@ void editorProcessKeypress() {
     editorInsertChar(c);
     break;
   }
+
+  quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
