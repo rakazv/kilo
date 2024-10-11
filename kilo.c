cd4ef79d995b01070e83351d01f78da528788582
rakazv <rakazv@outlook.com>
2024-10-11 11:21:07 +0900

    colorful search result

--- kilo.c
+++ kilo.c
@@ -37,7 +37,7 @@ enum editorKey {
   PAGE_DOWN
 };
 
-enum editorHighlight { HL_NORMAL = 0, HL_NUMBER };
+enum editorHighlight { HL_NORMAL = 0, HL_NUMBER, HL_MATCH };
 
 /*** data ***/
 
@@ -413,6 +413,8 @@ int editorSyntaxToColor(int hl) {
   switch (hl) {
   case HL_NUMBER:
     return 31;
+  case HL_MATCH:
+    return 34;
   default:
     return 37;
   }
@@ -533,6 +535,8 @@ void editorFindCallback(char *query, int key) {
       E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
+
+      memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
       break;
     }
   }
